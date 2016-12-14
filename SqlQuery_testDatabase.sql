1c9046d80c2d0f30e0f9cb9bd803f7ff525c2197
Malin Ardell <malin_ardell@hotmail.com>
2016-12-14 11:04:18 +0100

    Created a test database. Drop that base!

---
+++ SqlQuery_testDatabase.sql
@@ -0,0 +1,35 @@
+﻿--create database Frank
+go
+use Frank
+go
+
+create table Room (
+Id int identity primary key,
+Name varchar(64) unique not null,
+Capacity int not null,
+Has_WhiteBoard bit null,
+Has_Projector bit null,
+Has_TvScreen bit null
+)
+go
+
+create table Occupant(
+Id int identity primary key,
+FirstName varchar(64) not null,
+LastName varchar(64) not null,
+Email varchar(256) not null,
+UserRole varchar(64) not null
+)
+go
+
+create table Booking(
+    Id int identity primary key,
+    StartTime DateTime not null,
+    EndTime DateTime not null,
+    Room_Id int not null constraint FK_BookingRoom foreign key references Room(Id),
+    Occupant_Id int not null constraint FK_BookingOccupant foreign key references Occupant(Id),
+    Title varchar(64) not null,
+    Decription varchar(max) null,
+)
+go
+
