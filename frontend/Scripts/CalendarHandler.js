9f45a7dc40140848198f6d581efb48e98277d4ba
Malin Ardell <malin_ardell@hotmail.com>
2016-12-14 16:20:50 +0100

    kul jul

---
+++ frontend/Scripts/CalendarHandler.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultView: 'agendaWeek',
+        editable: true,
+        events: [
+    {
+        title: 'Möte',
+        start: '2016-12-15'
+    }
+        ]
+    });
+    $('#calendar').fullCalendar('addEventSource', source)
+});
