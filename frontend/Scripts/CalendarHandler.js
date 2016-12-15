764a081aeadf0e9f91990fe2f9b9ba8044c6f510
Malin Ardell <malin_ardell@hotmail.com>
2016-12-15 15:19:19 +0100

    15/12

--- frontend/Scripts/CalendarHandler.js
+++ frontend/Scripts/CalendarHandler.js
@@ -7,12 +7,43 @@
         },
         defaultView: 'agendaWeek',
         editable: true,
-        events: [
-    {
-        title: 'Möte',
-        start: '2016-12-15'
-    }
-        ]
+        viewRender: function (view, element) {
+            LoadEvents();
+        }
     });
-    $('#calendar').fullCalendar('addEventSource', source)
+
+    //LoadEvents();
+
 });
+
+//$('#calendar').fullCalendar().viewRender(function (view, element) {
+//    LoadEvents()
+
+//});
+
+//function($('#calendar').fullCalendar('getView'),)
+
+function LoadEvents() {
+    var roomId = 1;
+    var startTime = $('#calendar').fullCalendar('getView').intervalStart.format();
+    var endTime = $('#calendar').fullCalendar('getView').intervalEnd.format();
+    console.log(startTime + ' ' + endTime);
+
+
+    var url = 'http://localhost:18332/api/calendar/' + roomId + '/' + startTime + '/' + endTime;
+
+    $.getJSON(url, function (result) {
+        console.log(result);
+        var events = new Array();
+        for (var i = 0; i < result.length; i++) {
+            events.push({
+                id: result[i].id,
+                start: result[i].startTime.toLocaleString(),
+                end: result[i].endTime.toLocaleString(),
+                title: result[i].title
+            });
+        }
+        console.log(events);
+        $('#calendar').fullCalendar('renderEvents', events, true);
+    });
+}
