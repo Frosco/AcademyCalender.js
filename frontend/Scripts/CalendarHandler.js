1745a3ae7833d563fb58e5ef4281160ea391f639
Nils Grashorn <nils.grashorn@gmail.com>
2016-12-21 15:41:03 +0100

    Fixed the bug with displaying the same event multiple times. Added possibility to show different rooms.

--- frontend/Scripts/CalendarHandler.js
+++ frontend/Scripts/CalendarHandler.js
@@ -1,4 +1,29 @@
-﻿$(document).ready(function () {
+﻿
+
+var baseUrl = 'http://localhost:18332/api/calendar/';
+var roomList = new Array();
+
+$(document).ready(function () {
+
+    GetRooms();
+
+});
+
+function getCalendar(roomID) {
+
+    // Find the room with the ID passed into the function
+    var findRoom = function (roomID) {
+        for (var i = 0; i < roomList.length; i++) {
+            if (roomList[i].id === roomID) {
+                return roomList[i];
+            }
+        }
+    }
+
+    var room = findRoom(roomID);
+
+    console.log(room);
+
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -8,46 +33,63 @@
         defaultView: 'agendaWeek',
         editable: true,
         viewRender: function (view, element) {
-            LoadEvents();
+            LoadEvents(roomID);
         }
     });
+}
 
-    //LoadEvents();
 
-});
+function LoadEvents(roomID) {
 
-//$('#calendar').fullCalendar().viewRender(function (view, element) {
-//    LoadEvents()
-
-//});
-
-//function($('#calendar').fullCalendar('getView'),)
-
-function LoadEvents() {
-    var roomId = 1;
+    
+    
     var startTime = $('#calendar').fullCalendar('getView').intervalStart.format();
     var endTime = $('#calendar').fullCalendar('getView').intervalEnd.format();
-    console.log(startTime + ' ' + endTime);
 
-
-    var url = 'http://localhost:18332/api/calendar/' + roomId + '/' + startTime + '/' + endTime;
+    var url = baseUrl + roomID + '/' + startTime + '/' + endTime;
 
     $.getJSON(url, function (result) {
-        console.log(result);
-        var events = new Array();
         var existingEvents = $('#calendar').fullCalendar('clientEvents');
+        var existingEventIDs = new Array();
+
+        // Write the IDs of all existing events to a new array
+        $.each(existingEvents, function (index) {
+            existingEventIDs.push(this.id);
+        })
+
+        var events = new Array();
 
         for (var i = 0; i < result.length; i++) {
 
+            // Check if the event coming from the API is already displayed in the calendar (through checking with the ID list), otherwise add it.
+            if (($.inArray(result[i].id, existingEventIDs) === -1)) {
+                events.push({
+                    id: result[i].id,
+                    start: result[i].startTime.toLocaleString(),
+                    end: result[i].endTime.toLocaleString(),
+                    title: result[i].title
+                });
+            }
 
-            events.push({
-                id: result[i].id,
-                start: result[i].startTime.toLocaleString(),
-                end: result[i].endTime.toLocaleString(),
-                title: result[i].title
-            });
         }
-        console.log(events);
         $('#calendar').fullCalendar('renderEvents', events, true);
     });
+}
+
+function GetRooms() {
+    var roomUrl = baseUrl + '/rooms';
+
+    $.getJSON(roomUrl, function (result) {
+
+        // The room list is saved to an array accessible for all functions.
+        roomList = result;
+
+        for (var i = 0; i < result.length; i++) {
+            $('.dropdown-menu').append(
+                $('<li>').append(
+                $('<a>').attr('href', 'javascript:getCalendar(' + result[i].id + ')')
+                .append(result[i].name + ' (' + result[i].capacity + ')')
+                ));
+        }
+    })
 }
